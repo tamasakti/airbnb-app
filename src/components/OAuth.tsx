4ad79812935622dc743cbbdedc11446f898999dc
Muhammad Gumilang Tamasakti <gumilangtamasakti@gmail.com>
2023-05-28 14:01:15 +0700

    feat:add feature google sign in

--- src/components/OAuth.tsx
+++ src/components/OAuth.tsx
@@ -1,8 +1,44 @@
 import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+import { useNavigate } from "react-router";
 const OAuth = () => {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log(user);
+
+      //insert data to db firestore
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timeStamp: serverTimestamp(),
+        });
+      }
+      toast.success("Login Successful");
+      navigate("/");
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log(error);
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full py-3 text-sm font-medium text-white uppercase transition duration-150 ease-in-out bg-red-700 rounded shadow-md px-7 hover:bg-red-800 active:bg-red-900 hover:shadow-lg active:shadow-lg"
+    >
+      <FcGoogle className="mr-2 text-2xl bg-white rounded-full" />
       Continue with Google
     </button>
   );
